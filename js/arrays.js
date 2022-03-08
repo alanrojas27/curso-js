c69f74e82eedfb20de0138038ecf7fa9125035c6
Alan Rojas <alan_rojas27@outlook.com>
2022-03-08 17:13:17 -0300

    agregado de funciones para arrays

--- js/arrays.js
+++ js/arrays.js
@@ -4,3 +4,16 @@ function agregarMoneda() {
     console.table(valores)
 }
 
+function eliminarMoneda() {
+    debugger
+    nombreDeElemento = prompt("Ingrese la moneda que desea eliminar")
+    nombreDeElemento = nombreDeElemento.toUpperCase()
+    numeroDeElemento = valores.indexOf(nombreDeElemento)
+    if (numeroDeElemento >= 0) {
+    valores.splice(numeroDeElemento, 1)
+    console.log("Se elimino " + nombreDeElemento)
+    console.table(valores)
+    }else{
+        console.warn("No se encontro la moneda")
+    }
+}
